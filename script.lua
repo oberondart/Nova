adf2080bd8062d234b5193220b5c0e87cdbe4284
oberondart <durustein@gmail.com>
2025-12-05 15:45:38 -0500

    script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,15 @@
 local nova = require("nova")
+nova.let("my_string", "hello, world!")
+nova.out("my_string")
 
-nova.let("my_number", 123)
-nova.let("lucky", 12)
-nova.let("counter", 0)
+nova.let("my_number", 1)
+nova.let("this_number", 2)
+nova.let("happy_number", 3)
 
 nova.out("my_number")
-nova.out("lucky")
+nova.out("this_number")
+nova.out("happy_number")
 
 nova.array("my_array", {1, 2, 3, 4, 5})
 nova.out("my_array")
+nova.out("goodbye, world!")
